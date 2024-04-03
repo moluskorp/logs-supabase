fe7b73871c38f068464be8726cddf3583d8ae3f3
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-04-03 17:35:35 -0300

    Show

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,12 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { hello } from './controllers/hello'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/', hello)
+  // app.post('/', hello)
+
+  app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    console.log(request.body)
+
+    reply.status(200).send()
+  })
 }
